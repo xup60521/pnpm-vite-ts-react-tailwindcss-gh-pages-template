7143a803e5d3da2a05c59ae58ee32e3b54f674c4
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-21 22:36:07 +0800

    update text

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
             className="w-full min-h-screen bg-gradient-to-r from-fuchsia-500 to-cyan-500 flex items-center justify-center"
         >
             <h1 className="font-mono font-bold text-3xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                Vite + React + Typescript + SWC + TailwindCSS + GitHub Pages
+                Vite + React + Typescript + SWC + TailwindCSS + GitHub Pages w/ vitest
             </h1>
         </main>
     );
