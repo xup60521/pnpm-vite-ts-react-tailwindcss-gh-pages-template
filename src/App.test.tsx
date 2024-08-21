4e09c1479c9d30bdab72c7463be985b8a216cf36
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-21 22:35:31 +0800

    better format tailwindcss config; setup vitest

---
+++ src/App.test.tsx
@@ -0,0 +1,16 @@
+import { describe, expect, it } from "vitest";
+import { render, waitFor, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import App from "./App";
+
+describe("vitest is running", () => {
+    it("a test", async () => {
+        render(<App />);
+        const element = screen.getByTestId("app")
+        await userEvent.click(element);
+        await waitFor(() => {
+            expect(element).toBeInTheDocument();
+        });
+    });
+});
