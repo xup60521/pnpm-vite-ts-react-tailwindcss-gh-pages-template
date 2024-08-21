8171c4255ee9ed2a7a10c22a8f9acf7d2efcdf58
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-21 22:48:53 +0800

    update test id

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,7 +6,7 @@ import App from "./App";
 describe("vitest is running", () => {
     it("a test", async () => {
         render(<App />);
-        const element = screen.getByTestId("app")
+        const element = screen.getByTestId("test-app")
         await userEvent.click(element);
         await waitFor(() => {
             expect(element).toBeInTheDocument()
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
         <main
-            data-testid="app"
+            data-testid="test-app"
             className="w-full min-h-screen bg-gradient-to-r from-fuchsia-500 to-cyan-500 flex items-center justify-center"
         >
             <h1 className="font-mono font-bold text-3xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
